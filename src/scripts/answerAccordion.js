a86319fb08f8d2c86ca2676e1958d43fa75bd680
Anna Inozemets <118392560+anna-inozemets@users.noreply.github.com>
2023-04-06 09:36:28 +0300

    Update answerAccordion.js

--- src/scripts/answerAccordion.js
+++ src/scripts/answerAccordion.js
@@ -1,10 +1,9 @@
-var acc = document.getElementsByClassName('accordion__button');
-var i;
+const acc = document.getElementsByClassName('accordion__button');
 
-for (i = 0; i < acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener('click', function () {
     this.classList.toggle('active');
-    var panel = this.nextElementSibling;
+    const panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
