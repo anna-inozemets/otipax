6a60c2270fb3dc315ae4479670b9f665d9293a5f
Anna Inozemets <inoznetta@gmail.com>
2023-04-11 09:53:44 +0300

    Useful page was started(slider markup was implemented)

---
+++ src/pages/usefulInfo/scripts/usefulSlider.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('.useful__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 700,
+    autoplay: false,
+    autoplaySpeed: 3000,
+  });
+});
